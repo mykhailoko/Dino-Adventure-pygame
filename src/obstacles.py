1a30b62836cdae2ee5369005649200fca46ea5c5
mykhailoko <>
2024-03-27 19:35:39 +0300

    Отделили все препятствия в отдельный файл

---
+++ src/obstacles.py
@@ -0,0 +1,99 @@
+import pygame
+import os
+import random
+from assets import *
+
+pygame.init()
+
+os.environ['SDL_VIDEO_CENTERES'] = '1'
+info = pygame.display.Info()
+screen_width, screen_height = info.current_w, info.current_h
+window_width, window_height = screen_width - 3, screen_height - 60
+
+SCREEN_HEIGHT = screen_height
+SCREEN_WIDTH = screen_width
+
+
+class Cloud:
+    def __init__(self):
+        self.x = SCREEN_WIDTH + random.randint(800, 1000)
+        self.y = random.randint(50, 100)
+        self.image = CLOUD
+        self.width = self.image.get_width()
+
+    def update(self, background_speed):
+        self.x -= background_speed
+        if self.x < -self.width:
+            self.x = SCREEN_WIDTH + random.randint(2500, 3000)
+            self.y = random.randint(50, 100)
+
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image, (self.x, self.y))
+
+
+class Obstacle:
+    def __init__(self, image, type):
+        self.image = image
+        self.type = type
+        self.rect = self.image[self.type].get_rect()
+        self.rect.x = SCREEN_WIDTH
+
+    def update(self, obstacles, game_speed):
+        self.rect.x -= game_speed
+        if self.rect.x < -self.rect.width:
+            obstacles.pop()
+
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image[self.type], self.rect)
+
+
+class Zombi(Obstacle):
+    def __init__(self, image):
+        self.type = 0
+        super().__init__(image, self.type)
+        self.rect.y = 410
+        self.index = 0
+
+    def update(self, obstacles, game_speed):
+        self.rect.x -= (game_speed + 2)
+        if self.rect.x < -self.rect.width:
+            obstacles.pop()
+
+    def draw(self, SCREEN):
+        if self.index >= 9:
+            self.index = 0
+        SCREEN.blit(self.image[self.index // 5], self.rect)
+        self.index += 1
+
+
+class SmallCactus(Obstacle):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 435
+
+
+class LargeCactus(Obstacle):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 410
+
+
+class Bird(Obstacle):
+    def __init__(self, image):
+        self.type = 0
+        super().__init__(image, self.type)
+        self.rect.y = 310
+        self.index = 0
+
+    def update(self, obstacles, game_speed):
+        self.rect.x -= (game_speed + 5)
+        if self.rect.x < -self.rect.width:
+            obstacles.pop()
+
+    def draw(self, SCREEN):
+        if self.index >= 9:
+            self.index = 0
+        SCREEN.blit(self.image[self.index // 5], self.rect)
+        self.index += 1
