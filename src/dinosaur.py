204220665a035a3e39819e668b92adab7c46f629
mykhailoko <>
2024-05-08 23:55:41 +0300

    Добавили новые скины

--- src/dinosaur.py
+++ src/dinosaur.py
@@ -182,6 +182,72 @@ class DinosaurZombi(Dinosaur):
         self.dino_rect.x = self.X_POS
         self.dino_rect.y = self.Y_POS
 
+class DinosaurCowboy(Dinosaur):
+    X_POS = 80
+    Y_POS = 390
+    Y_POS_DUCK = 450
+    JUMP_VEL = 8.5
+
+    def __init__(self):
+        self.duck_img = DUCKING_COWBOY
+        self.run_img = RUNNING_COWBOY
+        self.jump_img = JUMPING_COWBOY
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
+class DinosaurViking(Dinosaur):
+    X_POS = 80
+    Y_POS = 380
+    Y_POS_DUCK = 435
+    JUMP_VEL = 8.5
+
+    def __init__(self):
+        self.duck_img = DUCKING_VIKING
+        self.run_img = RUNNING_VIKING
+        self.jump_img = JUMPING_VIKING
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
+class DinosaurClown(Dinosaur):
+    X_POS = 80
+    Y_POS = 390
+    Y_POS_DUCK = 450
+    JUMP_VEL = 8.5
+
+    def __init__(self):
+        self.duck_img = DUCKING_CLOWN
+        self.run_img = RUNNING_CLOWN
+        self.jump_img = JUMPING_CLOWN
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
 class DinosaurSkin1(Dinosaur):
     X_POS = 80
     Y_POS = 400
@@ -269,6 +335,227 @@ class DinosaurSkin1Zombi(Dinosaur):
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.X_POS
         self.dino_rect.y = self.Y_POS
+
+class DinosaurSkin1Cowboy(Dinosaur):
+    X_POS = 80
+    Y_POS = 400
+    Y_POS_DUCK = 445
+    JUMP_VEL = 8.5
+
+    def __init__(self):
+        self.duck_img = DUCKING_COWBOY_SKIN1
+        self.run_img = RUNNING_COWBOY_SKIN1
+        self.jump_img = JUMPING_COWBOY_SKIN1
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
+class DinosaurSkin1Viking(Dinosaur):
+    X_POS = 80
+    Y_POS = 380
+    Y_POS_DUCK = 435
+    JUMP_VEL = 8.5
+
+    def __init__(self):
+        self.duck_img = DUCKING_VIKING_SKIN1
+        self.run_img = RUNNING_VIKING_SKIN1
+        self.jump_img = JUMPING_VIKING_SKIN1
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
+class DinosaurSkin1Clown(Dinosaur):
+    X_POS = 80
+    Y_POS = 390
+    Y_POS_DUCK = 445
+    JUMP_VEL = 8.5
+
+    def __init__(self):
+        self.duck_img = DUCKING_CLOWN_SKIN1
+        self.run_img = RUNNING_CLOWN_SKIN1
+        self.jump_img = JUMPING_CLOWN_SKIN1
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
+class DinosaurSkin3(Dinosaur):
+    X_POS = 80
+    Y_POS = 400
+    Y_POS_DUCK = 445
+    JUMP_VEL = 8.5
+
+    def __init__(self):
+        self.duck_img = DUCKING_SKIN3
+        self.run_img = RUNNING_SKIN3
+        self.jump_img = JUMPING_SKIN3
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
+class DinosaurSkin3Winter(Dinosaur):
+    X_POS = 80
+    Y_POS = 400
+    Y_POS_DUCK = 445
+    JUMP_VEL = 8.5
+
+    def __init__(self):
+        self.duck_img = DUCKING_WINTER_SKIN3
+        self.run_img = RUNNING_WINTER_SKIN3
+        self.jump_img = JUMPING_WINTER_SKIN3
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
+class DinosaurSkin3Beach(Dinosaur):
+    X_POS = 80
+    Y_POS = 400
+    Y_POS_DUCK = 445
+    JUMP_VEL = 8.5
+
+    def __init__(self):
+        self.duck_img = DUCKING_BEACH_SKIN3
+        self.run_img = RUNNING_BEACH_SKIN3
+        self.jump_img = JUMPING_BEACH_SKIN3
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
+class DinosaurSkin3Zombi(Dinosaur):
+    X_POS = 80
+    Y_POS = 400
+    Y_POS_DUCK = 445
+    JUMP_VEL = 8.5
+
+    def __init__(self):
+        self.duck_img = DUCKING_SKIN3_ZOMBI
+        self.run_img = RUNNING_SKIN3_ZOMBI
+        self.jump_img = JUMPING_SKIN3_ZOMBI
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
+class DinosaurSkin3Cowboy(Dinosaur):
+    X_POS = 80
+    Y_POS = 400
+    Y_POS_DUCK = 445
+    JUMP_VEL = 8.5
+
+    def __init__(self):
+        self.duck_img = DUCKING_COWBOY_SKIN3
+        self.run_img = RUNNING_COWBOY_SKIN3
+        self.jump_img = JUMPING_COWBOY_SKIN3
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
+class DinosaurSkin3Viking(Dinosaur):
+    X_POS = 80
+    Y_POS = 380
+    Y_POS_DUCK = 435
+    JUMP_VEL = 8.5
+
+    def __init__(self):
+        self.duck_img = DUCKING_VIKING_SKIN3
+        self.run_img = RUNNING_VIKING_SKIN3
+        self.jump_img = JUMPING_VIKING_SKIN3
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
+class DinosaurSkin3Clown(Dinosaur):
+    X_POS = 80
+    Y_POS = 395
+    Y_POS_DUCK = 440
+    JUMP_VEL = 8.5
+
+    def __init__(self):
+        self.duck_img = DUCKING_CLOWN_SKIN3
+        self.run_img = RUNNING_CLOWN_SKIN3
+        self.jump_img = JUMPING_CLOWN_SKIN3
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
 
 class DinosaurMultiplayer1(Dinosaur):
     X_POS = 80
