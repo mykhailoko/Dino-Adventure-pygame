8017c3d2b65be33a1ba9ce877d0e6e7e2a11aca6
exgraal7 <144822299+exgraal7@users.noreply.github.com>
2024-06-03 21:12:18 +0300

    Добавили звук нажатия на кнопку

--- src/button.py
+++ src/button.py
@@ -4,7 +4,7 @@ import os
 pygame.init()
 pygame.mixer.init()
 
-os.environ['SDL_VIDEO_CENTERES'] = '1' 
+os.environ['SDL_VIDEO_CENTERED'] = '1' 
 info = pygame.display.Info()
 screen_width, screen_height = info.current_w, info.current_h
 window_width, window_height = screen_width, screen_height - 10
@@ -13,6 +13,8 @@ SCREEN_HEIGHT = screen_height
 SCREEN_WIDTH = screen_width
 SCREEN = pygame.display.set_mode((window_width, window_height), pygame.RESIZABLE)
 pygame.display.set_caption("Dino Adventure")
+
+BUTTON_CLICK_SOUND = pygame.mixer.Sound(os.path.join("Assets", "Sounds", "button_click.mp3"))
 
 class Button():
     def __init__(self, x, y, image):
@@ -23,17 +25,17 @@ class Button():
 
     def draw(self):
         action = False
-
         pos = pygame.mouse.get_pos()
-
         if self.rect.collidepoint(pos):
             if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
                 self.clicked = True
+                BUTTON_CLICK_SOUND.play()
                 action = True
-
         if pygame.mouse.get_pressed()[0] == 0:
             self.clicked = False
-
         SCREEN.blit(self.image, (self.rect.x, self.rect.y))
-
         return action
+
+    def is_hovered(self):
+        pos = pygame.mouse.get_pos()
+        return self.rect.collidepoint(pos)
